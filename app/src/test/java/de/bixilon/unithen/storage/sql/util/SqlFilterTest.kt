7af803107e512c792d42134909a1e4fe6606e865
Moritz Zwerger <bixilon@bixilon.de>
2026-01-24 19:09:31 +0100

    sql filter tests

---
+++ app/src/test/java/de/bixilon/unithen/storage/sql/util/SqlFilterTest.kt
@@ -0,0 +1,31 @@
+package de.bixilon.unithen.storage.sql.util
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class SqlFilterTest {
+
+    @Test
+    fun `AND empty filter`() {
+        val filter = SqlFilter.and()
+        val expected = SqlFilter("", listOf())
+
+        assertEquals(filter, expected)
+    }
+
+    @Test
+    fun `AND single filter`() {
+        val filter = SqlFilter.and("abc" to "def")
+        val expected = SqlFilter("abc=?", listOf("def"))
+
+        assertEquals(filter, expected)
+    }
+
+    @Test
+    fun `AND multiple filters`() {
+        val filter = SqlFilter.and("abc" to "def", "xyz" to null, "ush" to 1)
+        val expected = SqlFilter("abc=? AND ush=?", listOf("def", "1"))
+
+        assertEquals(filter, expected)
+    }
+}
