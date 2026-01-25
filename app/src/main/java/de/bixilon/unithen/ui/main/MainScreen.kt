44a3533edcbc47972514a6cf4d950083be3845f7
Moritz Zwerger <bixilon@bixilon.de>
2026-01-25 23:52:38 +0100

    wip setup screen

--- app/src/main/java/de/bixilon/unithen/ui/main/MainScreen.kt
+++ app/src/main/java/de/bixilon/unithen/ui/main/MainScreen.kt
@@ -3,13 +3,23 @@ package de.bixilon.unithen.ui.main
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import de.bixilon.unithen.storage.DataStorage
+import de.bixilon.unithen.ui.navigation.LocalNavigation
 
 
 @Composable
 fun MainScreen() {
+    val navigator = LocalNavigation.current
+
+    LaunchedEffect(Unit) {
+        if (DataStorage.STORAGE.accounts.count == 0) {
+            navigator.navigate(SetupRoute)
+        }
+    }
+
 
     Row {
-        Text("Hi\nPlease login!")
-        SitesScreen()
+        Text("Welcome!")
     }
 }
