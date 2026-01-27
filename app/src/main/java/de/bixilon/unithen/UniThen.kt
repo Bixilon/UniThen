76d85b3d81b4e2ff80d2d973ee880e4460a7baf5
Moritz Zwerger <bixilon@bixilon.de>
2026-01-27 18:53:52 +0100

    copyright, GPLv3+ licence

--- app/src/main/java/de/bixilon/unithen/UniThen.kt
+++ app/src/main/java/de/bixilon/unithen/UniThen.kt
@@ -1,3 +1,15 @@
+/*
+ * UniThen
+ * Copyright (C) 2026 Moritz Zwerger
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * This software is not affiliated with UniNow GmbH, the provider/developer of the booking system.
+ */
+
 package de.bixilon.unithen
 
 import android.app.Application
--- app/src/main/java/de/bixilon/unithen/api/user/SiteDetails.kt
+++ app/src/main/java/de/bixilon/unithen/api/user/SiteDetails.kt
@@ -1,3 +1,15 @@
+/*
+ * UniThen
+ * Copyright (C) 2026 Moritz Zwerger
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * This software is not affiliated with UniNow GmbH, the provider/developer of the booking system.
+ */
+
 package de.bixilon.unithen.api.user
 
 import de.bixilon.kutil.stream.InputStreamUtil.readAll
@@ -6,8 +18,6 @@ import de.bixilon.unithen.api.HttpUtil
 import okhttp3.OkHttpClient
 import org.jsoup.Jsoup
 import java.net.URI
-
-typealias SHA1 = String
 
 data class SiteDetails(
     val name: String,
