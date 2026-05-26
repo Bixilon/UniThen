ea306daf2bccb4cc8e3e6483ac1c6fdfa1d09085
Moritz Zwerger <bixilon@bixilon.de>
2026-05-27 01:34:57 +0200

    move checkin migrations to db version 6
    5 was chery picked into master

--- app/src/main/java/de/bixilon/unithen/storage/sql/SqlHelper.kt
+++ app/src/main/java/de/bixilon/unithen/storage/sql/SqlHelper.kt
@@ -35,7 +35,7 @@ class SqlHelper(context: Context) : SQLiteOpenHelper(context, NAME, null, VERSIO
 
     companion object {
         const val NAME = "uninow"
-        const val VERSION = 5
+        const val VERSION = 6
 
         fun SQLiteDatabase.executeBatch(path: String) {
             val schema = SqlHelper::class.java.getResourceAsStream("/sql/$path.sql")!!.readAsString().split(";").map { it.removeSuffix("\n") }.filter { it.isNotBlank() }
