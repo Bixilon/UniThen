5412f5e8e44dc5fa4ec0da09a4fa3b4e9beb80c0
Moritz Zwerger <bixilon@bixilon.de>
2026-05-26 23:58:19 +0200

    appointment checkins

--- app/src/main/resources/sql/schema.sql
+++ app/src/main/resources/sql/schema.sql
@@ -120,9 +120,29 @@ CREATE TABLE course_enrolled (
 
 CREATE TABLE appointment_attendee (
   user INTEGER,
-  course INTEGER,
+  appointment INTEGER,
 
-  PRIMARY KEY (user, course),
+  PRIMARY KEY (user, appointment),
   FOREIGN KEY (user) REFERENCES users(id),
-  FOREIGN KEY (course) REFERENCES courses(id)
+  FOREIGN KEY (appointment) REFERENCES appointments(id)
+);
+
+CREATE TABLE appointment_checkins (
+  user INTEGER,
+  appointment INTEGER,
+
+  time INTEGER NULL,
+  uuid VARCHAR(36) NULL,
+
+  message VARCHAR(1024) NULL,
+
+  sync INTEGER NULL,
+
+  status TEXT CHECK(status IN ('OK', 'FAILED', 'PENDING')),
+
+
+  PRIMARY KEY (user, appointment),
+  FOREIGN KEY (user) REFERENCES users(id),
+  FOREIGN KEY (appointment) REFERENCES appointments(id),
+  UNIQUE (appointment, uuid)
 );
