55e82039edeb06a0308a3ad5b7e97dd1d406cbb5
Moritz Zwerger <bixilon@bixilon.de>
2026-07-02 16:27:27 +0200

    present screen: move qr code up and make scrollable
    Otherwise small devices might not display the qr code correct.

--- app/src/main/java/de/bixilon/unithen/ui/main/checkin/present/PresentQrScreen.kt
+++ app/src/main/java/de/bixilon/unithen/ui/main/checkin/present/PresentQrScreen.kt
@@ -17,7 +17,9 @@ import android.content.Context
 import android.view.WindowManager
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -68,34 +70,18 @@ fun PresentQrScreen(account: Account, course: Course, appointment: Appointment) 
     }
 
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
     ) {
         Text(
             text = course.name,
             style = MaterialTheme.typography.headlineLarge,
             fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Center,
-            modifier = Modifier.padding(16.dp),
+            modifier = Modifier.padding(16.dp)
         )
-
-        Spacer(modifier = Modifier.height(24.dp))
-
-
-        InfoContainer(modifier = Modifier
-            .padding(horizontal = 16.dp)
-            .fillMaxWidth(0.8f)) {
-            InfoPair(R.string.course_name.i18n(), account.fullname)
-            InfoPair(R.string.appointment_start.i18n(), appointment.start.format())
-            InfoPair(R.string.appointment_end.i18n(), appointment.end.format())
-            InfoPair(R.string.appointment_location.i18n(), appointment.location)
-            if (BuildConfig.DEBUG) {
-                InfoPair("ID", appointment.uuid.toString())
-            }
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
 
         Box(Modifier.padding(4.dp)) {
             val (firstname, lastname) = if (name) Pair("A", "B") else Pair(account.firstname, account.lastname)
@@ -116,6 +102,20 @@ fun PresentQrScreen(account: Account, course: Course, appointment: Appointment) 
             style = MaterialTheme.typography.bodySmall,
             color = MaterialTheme.colorScheme.onSurface,
         )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+
+        InfoContainer(modifier = Modifier
+            .padding(horizontal = 16.dp)) {
+            InfoPair(R.string.course_name.i18n(), account.fullname)
+            InfoPair(R.string.appointment_start.i18n(), appointment.start.format())
+            InfoPair(R.string.appointment_end.i18n(), appointment.end.format())
+            InfoPair(R.string.appointment_location.i18n(), appointment.location)
+            if (BuildConfig.DEBUG) {
+                InfoPair("ID", appointment.uuid.toString())
+            }
+        }
     }
 }
 
