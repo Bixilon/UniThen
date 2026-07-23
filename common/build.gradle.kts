97a0e63c38be11cb7264b066c9798282477c9c0e
Moritz Zwerger <bixilon@bixilon.de>
2026-07-23 22:59:32 +0200

    native sqlite driver

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -148,8 +148,9 @@ kotlin {
             implementation(libs.kotlinx.coroutines.test)
         }
 
-        iosMain.dependencies {
+        nativeMain.dependencies {
             implementation(libs.zxingcpp.native)
+            implementation(libs.sqliter.driver)
         }
     }
 }
---
+++ common/src/nativeMain/kotlin/de/bixilon/unithen/storage/sql/NativeSQLiteHelper.kt
@@ -0,0 +1,113 @@
+/*
+ * UniThen
+ * Copyright (C) 2026 Moritz Zwerger
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * This software is not affiliated with UniNow GmbH, the provider/developer of the booking system.
+ */
+
+package de.bixilon.unithen.storage.sql
+
+import co.touchlab.sqliter.*
+import kotlin.time.Instant
+import kotlin.uuid.Uuid
+
+class NativeSQLiteHelper : SQLiteHelper {
+    val driver by lazy { createDatabaseManager(DatabaseConfiguration("uninow", SqlStorage.VERSION, create = this::create, upgrade = this::upgrade)).createMultiThreadedConnection() }
+
+
+    private fun DatabaseConnection.executeBatch(path: String) {
+        val statements = SqlUtil.split(SqlUtil.load(path))
+        withTransaction { statements.forEach { execute(it) } }
+    }
+
+    private fun create(database: DatabaseConnection) {
+        database.executeBatch("schema")
+    }
+
+    private fun upgrade(database: DatabaseConnection, start: Int, end: Int) = database.withTransaction {
+        for (version in (start + 1)..end) {
+            try {
+                database.executeBatch("migrations/${version}")
+            } catch (error: Throwable) {
+                throw Exception("Error during database migration $version: ${error.message}", error)
+            }
+        }
+    }
+
+    override fun load() {
+        driver
+    }
+
+    private fun Statement.bind(vararg parameters: Any?) {
+        for ((index, parameter) in parameters.withIndex()) {
+            val actual = index + 1
+            when (parameter) {
+                null -> bindNull(actual)
+                is Int -> bindLong(actual, parameter.toLong())
+                is Long -> bindLong(actual, parameter)
+                is String -> bindString(actual, parameter)
+                is Instant -> bindLong(actual, parameter.epochSeconds)
+                is Uuid -> bindString(actual, parameter.toString())
+                is ByteArray -> bindBlob(actual, parameter)
+                is Enum<*> -> bindString(actual, parameter.name)
+                else -> throw IllegalArgumentException("Unknown parameter type: $parameter")
+            }
+        }
+    }
+
+    private fun createStatement(sql: String, vararg parameters: Any?): Statement {
+        val statement = driver.createStatement(sql)
+        statement.bind(*parameters)
+
+        return statement
+    }
+
+
+    override fun query(sql: String, vararg parameters: Any?): SQLiteHelper.Cursor {
+        val statement = createStatement(sql, *parameters)
+
+        return NativeCursor(statement, statement.query())
+    }
+
+    override fun execute(sql: String, vararg parameters: Any?): Int {
+        val statement = createStatement(sql, *parameters)
+
+        return statement.executeUpdateDelete()
+    }
+
+    override fun insert(sql: String, vararg parameters: Any?): Int {
+        val statement = createStatement(sql, *parameters)
+
+        return statement.executeInsert().toInt() // TODO: This returns the rowid, not the auto increment id
+    }
+
+    override fun <T> transaction(block: () -> T) = driver.withTransaction { block.invoke() }
+
+    override fun close() {
+        driver.close()
+    }
+
+    class NativeCursor(val statement: Statement, val cursor: Cursor) : SQLiteHelper.Cursor {
+        override fun getBlob(index: Int) = cursor.getBytes(index + 1)
+        override fun getBlobOrNull(index: Int) = cursor.getBytes(index + 1)
+
+        override fun getString(index: Int) = cursor.getString(index + 1)
+        override fun getStringOrNull(index: Int) = cursor.getString(index + 1)
+
+        override fun getInt(index: Int) = cursor.getLong(index + 1).toInt()
+        override fun getLong(index: Int) = cursor.getLong(index + 1)
+
+        override fun isNull(index: Int) = cursor.isNull(index + 1)
+
+        override fun moveToNext() = cursor.next()
+
+        override fun close() = statement.finalizeStatement()
+
+        override fun isEmpty() = !cursor.next()
+    }
+}
