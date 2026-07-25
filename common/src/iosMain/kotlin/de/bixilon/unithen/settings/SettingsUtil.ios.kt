cce17a60cfb13388a648310f7cb562a55a08a284
Moritz Zwerger <bixilon@bixilon.de>
2026-07-25 02:22:08 +0200

    ios: save settings

--- common/src/iosMain/kotlin/de/bixilon/unithen/settings/SettingsUtil.ios.kt
+++ common/src/iosMain/kotlin/de/bixilon/unithen/settings/SettingsUtil.ios.kt
@@ -1,28 +1,95 @@
 package de.bixilon.unithen.settings
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import de.bixilon.kutil.enums.ValuesEnum
+import platform.Foundation.NSUserDefaults
+
+
+private operator fun NSUserDefaults.contains(setting: Setting<*>): Boolean {
+    return objectForKey(setting.key) == null
+}
+
+private operator fun NSUserDefaults.get(setting: Setting<Boolean>): Boolean {
+    if (setting !in this) return setting.default
+
+    return boolForKey(setting.key)
+}
+
+private operator fun NSUserDefaults.get(setting: Setting<Int>): Int {
+    if (setting !in this) return setting.default
+
+    return integerForKey(setting.key).toInt()
+}
+
+private operator fun NSUserDefaults.get(setting: Setting<String>): String {
+    if (setting !in this) return setting.default
+
+    return stringForKey(setting.key) ?: setting.default
+}
+
+private operator fun <T : Enum<T>> NSUserDefaults.get(setting: Setting<T>, values: ValuesEnum<T>): T {
+    if (setting !in this) return setting.default
+
+    return stringForKey(setting.key)?.let { values[it] } ?: setting.default
+}
+
 
 @Composable
 actual fun rememberSetting(setting: Setting<Boolean>): MutableState<Boolean> {
-    return remember { mutableStateOf(setting.default) }
+    val defaults = NSUserDefaults.standardUserDefaults
+    val store = remember { mutableStateOf(defaults[setting]) }
+
+    LaunchedEffect(store) {
+        defaults.setBool(store.value, setting.key)
+    }
+
+    return store
 }
 
 @Composable
 actual fun <T : Enum<T>> rememberSetting(setting: Setting<T>, values: ValuesEnum<T>): MutableState<T> {
-    return remember { mutableStateOf(setting.default) }
+    val defaults = NSUserDefaults.standardUserDefaults
+    val store = remember { mutableStateOf(defaults[setting, values]) }
+
+    LaunchedEffect(store) {
+        defaults.setObject(store.value.name, setting.key)
+    }
+
+    return remember {
+        object : MutableState<T> {
+            override var value: T
+                get() = store.value
+                set(next) {
+                    store.value = next
+                }
+
+            override fun component1() = value
+            override fun component2(): (T) -> Unit = { this.value = it }
+        }
+    }
 }
 
 @Composable
 actual fun rememberSetting(setting: Setting<Int>): MutableState<Int> {
-    return remember { mutableStateOf(setting.default) }
+    val defaults = NSUserDefaults.standardUserDefaults
+    val store = remember { mutableStateOf(defaults[setting]) }
+
+    LaunchedEffect(store) {
+        defaults.setInteger(store.value.toLong(), setting.key)
+    }
+
+    return store
 }
 
 @Composable
 actual fun rememberSetting(setting: Setting<String>): MutableState<String> {
-    return remember { mutableStateOf(setting.default) }
+    val defaults = NSUserDefaults.standardUserDefaults
+    val store = remember { mutableStateOf(defaults[setting]) }
+
+    LaunchedEffect(store) {
+        defaults.setObject(store.value, setting.key)
+    }
+
+    return store
 }
 
