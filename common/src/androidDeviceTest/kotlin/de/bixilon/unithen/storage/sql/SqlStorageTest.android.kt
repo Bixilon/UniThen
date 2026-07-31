70aafb08ecacb5501e82a846c4fe01aa37787075
Moritz Zwerger <bixilon@bixilon.de>
2026-08-01 00:48:28 +0200

    fix android host sql tests

--- common/src/androidDeviceTest/kotlin/de/bixilon/unithen/storage/sql/SqlStorageTest.android.kt
+++ common/src/androidDeviceTest/kotlin/de/bixilon/unithen/storage/sql/SqlStorageTest.android.kt
@@ -1,13 +1,14 @@
 package de.bixilon.unithen.storage.sql
 
-import java.io.File
+import androidx.test.platform.app.InstrumentationRegistry
 import java.io.FileOutputStream
 
 actual fun ByteArray.copyTo(path: String) {
-    File("databases").mkdirs()
-    FileOutputStream("databases/$path").use { it.write(this) }
+    val file = InstrumentationRegistry.getInstrumentation().context.getDatabasePath(path)
+    file.parentFile!!.mkdirs()
+    FileOutputStream(file).use { it.write(this) }
 }
 
 actual fun delete(path: String) {
-    File("databases/$path").delete()
+    InstrumentationRegistry.getInstrumentation().context.getDatabasePath(path).delete()
 }
