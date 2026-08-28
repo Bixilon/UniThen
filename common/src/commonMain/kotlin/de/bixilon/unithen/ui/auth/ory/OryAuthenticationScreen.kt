93d318e67407dd726fad67b0e092e78b98ae422e
Moritz Zwerger <bixilon@bixilon.de>
2026-08-28 13:37:20 +0200

    ory authentication: improve flow fetch error page

--- common/src/commonMain/kotlin/de/bixilon/unithen/ui/auth/ory/OryAuthenticationScreen.kt
+++ common/src/commonMain/kotlin/de/bixilon/unithen/ui/auth/ory/OryAuthenticationScreen.kt
@@ -61,8 +61,15 @@ private fun FlowError(host: String) {
     val navigation = LocalNavigation.current
 
     Screen {
+        Spacer(Modifier.height(100.dp))
         ErrorBox(Res.string.auth_error_flow_title.i18n(), Res.string.auth_error_flow_description.i18n())
-        Button({ navigation.navigate(LegacyAuthenticationRoute(host)) }, modifier = Modifier.fillMaxWidth()) { Text(Res.string.auth_try_legacy.i18n()) }
+        Spacer(Modifier.height(20.dp))
+
+        Button({ navigation.navigate(LegacyAuthenticationRoute(host)) }, modifier = Modifier.fillMaxWidth(), colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.onSecondaryContainer)) {
+            Icon(Icons.Filled.Warning, "")
+            Spacer(Modifier.width(4.dp))
+            Text(Res.string.auth_try_legacy.i18n())
+        }
     }
 }
 
