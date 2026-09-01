fdeb75351187d83562fa311fcb77b73843e57a03
Moritz Zwerger <bixilon@bixilon.de>
2026-09-01 19:18:45 +0200

    courses: cache icon
    This makes compose not flicker during fetching (or recomposition in general)

--- common/src/commonMain/kotlin/de/bixilon/unithen/ui/main/courses/CoursesScreen.kt
+++ common/src/commonMain/kotlin/de/bixilon/unithen/ui/main/courses/CoursesScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -91,9 +92,10 @@ fun CoursesScreen() {
                         item(key = "e" + event.id) {
                             val site = rememberStorage { sites[event.site] } // TODO: Section?
                             Row(verticalAlignment = Alignment.CenterVertically) {
-                                if (site.icon != null) {
+                                val icon = remember { site.icon } // prevents flickering of icon during recomposition (async image)
+                                if (icon != null) {
                                     AsyncImage(
-                                        site.icon,
+                                        icon,
                                         contentDescription = "Site icon",
                                         modifier = Modifier
                                             .size(24.dp)
