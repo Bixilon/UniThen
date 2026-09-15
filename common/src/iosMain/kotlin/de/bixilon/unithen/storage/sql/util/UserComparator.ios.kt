484a59f8a4c15c7f61d13e664f475d146474bc7b
Moritz Zwerger <bixilon@bixilon.de>
2026-09-15 14:13:32 +0200

    cleanup ios user comparator

--- common/src/iosMain/kotlin/de/bixilon/unithen/storage/sql/util/UserComparator.ios.kt
+++ common/src/iosMain/kotlin/de/bixilon/unithen/storage/sql/util/UserComparator.ios.kt
@@ -1,13 +1,11 @@
 package de.bixilon.unithen.storage.sql.util
 
+import de.bixilon.kutil.cast.CastUtil.cast
 import platform.Foundation.NSCaseInsensitiveSearch
 import platform.Foundation.NSDiacriticInsensitiveSearch
 import platform.Foundation.NSString
 import platform.Foundation.compare
 
-@Suppress("CAST_NEVER_SUCCEEDS")
 actual fun compare(a: String, b: String): Int {
-    val _a = a as NSString
-
-    return _a.compare(b, options = NSCaseInsensitiveSearch or NSDiacriticInsensitiveSearch).toInt() // TODO: Test
+    return a.cast<NSString>().compare(b, options = NSCaseInsensitiveSearch or NSDiacriticInsensitiveSearch).toInt() // TODO: Test
 }
