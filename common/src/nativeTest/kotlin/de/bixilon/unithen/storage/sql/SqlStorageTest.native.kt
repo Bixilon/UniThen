5285598de0098dd3954beefcf98f9440a1a438a2
Moritz Zwerger <bixilon@bixilon.de>
2026-09-17 14:24:22 +0200

    fix ios sql migration tests

--- common/src/nativeTest/kotlin/de/bixilon/unithen/storage/sql/SqlStorageTest.native.kt
+++ common/src/nativeTest/kotlin/de/bixilon/unithen/storage/sql/SqlStorageTest.native.kt
@@ -1,5 +1,6 @@
 package de.bixilon.unithen.storage.sql
 
+import co.touchlab.sqliter.DatabaseFileContext
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.addressOf
 import kotlinx.cinterop.usePinned
@@ -9,14 +10,17 @@ import platform.posix.fwrite
 import platform.posix.unlink
 
 actual fun delete(path: String) {
-    unlink(path)
+    val actual = DatabaseFileContext.databasePath(path, null)
+    unlink(actual)
 }
 
 @OptIn(ExperimentalForeignApi::class)
 actual fun ByteArray.copyTo(path: String) {
+    val actual = DatabaseFileContext.databasePath(path, null)
+
     usePinned { pinned ->
-        val file = fopen(path, "wb")!!
-        fwrite(pinned.addressOf(0), 1u, size.toULong(), file)
-        fclose(file)
+        val fd = fopen(actual, "wb")!!
+        fwrite(pinned.addressOf(0), 1u, size.toULong(), fd)
+        fclose(fd)
     }
 }
