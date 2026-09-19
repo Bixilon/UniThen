59ecffe0ff509242c1f9f38440ea334970baadcc
Moritz Zwerger <bixilon@bixilon.de>
2026-09-19 16:54:15 +0200

    main scaffold: use Surface instead of Box
    Makes the font colors correct

--- common/src/commonMain/kotlin/de/bixilon/unithen/ui/MainScaffold.kt
+++ common/src/commonMain/kotlin/de/bixilon/unithen/ui/MainScaffold.kt
@@ -1,19 +1,18 @@
 package de.bixilon.unithen.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 
 @Composable
 fun MainScaffold(content: @Composable () -> Unit) {
-    Box(
+    Surface(
+        color = MaterialTheme.colorScheme.background,
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
     ) {
         content.invoke()
     }
